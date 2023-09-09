ca2b057d2e1b6768afb6adf90e54ed07f466f8cf
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-09 09:19:48 +0300

    setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('prettier').Config}
+ */
+ module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  htmlWhitespaceSensitivity: 'ignore',
+  plugins: [require.resolve('prettier-plugin-organize-attributes')],
+  attributeGroups: [
+      '$ANGULAR_STRUCTURAL_DIRECTIVE',
+      '$ANGULAR_ELEMENT_REF',
+      '$ID',
+      '$DEFAULT',
+      '$CLASS',
+      '^\\[class\\.',
+      '$ANGULAR_ANIMATION',
+      '$ANGULAR_ANIMATION_INPUT',
+      '$ANGULAR_INPUT',
+      '$ANGULAR_TWO_WAY_BINDING',
+      '$ANGULAR_OUTPUT',
+  ],
+  overrides: [
+      {
+          files: ['*.html'],
+          options: {parser: 'html'},
+      },
+      {
+          files: ['*.component.html', '*.template.html'],
+          options: {parser: 'angular'},
+      },
+  ],
+};
