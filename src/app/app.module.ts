42a576726556b943ba3b158b6b9ed32379f074ac
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-09 10:24:57 +0300

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+/* eslint-disable no-param-reassign */
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, ButtonModule, RippleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
