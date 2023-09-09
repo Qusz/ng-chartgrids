d149a68472a360368218309ed9c6a8e64999375e
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-09 16:58:04 +0300

    configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { GridsComponent } from './pages/grids/grids.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/charts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'charts',
+    component: ChartsComponent
+  },
+  {
+    path: 'grids',
+    component: GridsComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/charts'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
