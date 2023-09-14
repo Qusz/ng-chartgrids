7e761aeff7e18ac05bdcf63856695e67da508b9d
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-14 21:19:06 +0300

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { PrimeNGConfig } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 
-import { PrimeNGConfig } from 'primeng/api';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { DefaultLayoutModule } from './components/default-layout/default-layout.module';
+import { HeaderModule } from './components/header/header.module';
+import { SidebarModule } from './components/sidebar/sidebar.module';
+import { GraphQLModule } from './graphql.module';
 
 const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
   primeConfig.ripple = true;
@@ -17,7 +24,18 @@ const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, ButtonModule, RippleModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    DefaultLayoutModule,
+    HeaderModule,
+    SidebarModule,
+    RouterModule,
+    GraphQLModule
+  ],
   providers: [
     {
       provide: APP_INITIALIZER,
