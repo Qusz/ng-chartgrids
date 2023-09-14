1964ad7fea7b84e49ad97b3ad536ccbd80545ec0
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-14 21:20:01 +0300

    add type

---
+++ src/app/models/chart-data.type.ts
@@ -0,0 +1,11 @@
+export type ChartDataCategory = {
+  value: string;
+  count: number;
+};
+
+export type ChartDataDate = {
+  date: number;
+  value: number;
+};
+
+export type ChartData = ChartDataCategory | ChartDataDate;
