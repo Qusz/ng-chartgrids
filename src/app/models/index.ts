a8524ca0672775f5fc2119fbae4880acb827fac0
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-14 21:20:16 +0300

    add models index file

---
+++ src/app/models/index.ts
@@ -0,0 +1,15 @@
+import type { ChartType } from './chart.type';
+import type { SidebarItem } from './sidebar-item.type';
+import type { Customer } from './customer.type';
+import type { CustomersQuery } from './customers-query.type';
+import type { ChartData, ChartDataCategory, ChartDataDate } from './chart-data.type';
+
+export type {
+  SidebarItem,
+  Customer,
+  CustomersQuery,
+  ChartData,
+  ChartType,
+  ChartDataCategory,
+  ChartDataDate
+};
