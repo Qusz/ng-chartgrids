718c5482d2ef5c7214753b0305de20a7439a9628
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-15 11:01:56 +0300

    add data-specific methods

--- src/app/shared/get-data/get-data.service.ts
+++ src/app/shared/get-data/get-data.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 
-import type { CustomersQuery } from 'src/app/models';
+import type {
+  CustomersQuery,
+  GendersQuery,
+  PointOfRegistrartionQuery,
+  RegistrationDateQuery
+} from 'src/app/models/query.type';
 
-import { Apollo } from 'apollo-angular';
+import { Apollo, TypedDocumentNode } from 'apollo-angular';
 import { Observable, map } from 'rxjs';
 
-import { GET_ALL_CUSTOMERS } from 'src/app/queries/get-all';
+import {
+  GET_ALL_CUSTOMERS,
+  GET_GENDERS,
+  GET_DATES_OF_REGISTRATION,
+  GET_POINTS_OF_REGISTRATION
+} from 'src/app/queries';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +23,27 @@ import { GET_ALL_CUSTOMERS } from 'src/app/queries/get-all';
 export class GetDataService {
   constructor(private apollo: Apollo) {}
 
-  getCustomers(): Observable<CustomersQuery> {
-    return this.apollo
-      .watchQuery({
-        query: GET_ALL_CUSTOMERS
-      })
-      .valueChanges.pipe(map((data) => data as CustomersQuery));
+  getCustomers() {
+    return this.getData<CustomersQuery>(GET_ALL_CUSTOMERS);
+  }
+
+  getGenders() {
+    return this.getData<GendersQuery>(GET_GENDERS);
+  }
+
+  getPointsOfRegistration() {
+    return this.getData<PointOfRegistrartionQuery>(GET_POINTS_OF_REGISTRATION);
+  }
+
+  getDatesOfRegistrartion() {
+    return this.getData<RegistrationDateQuery>(GET_DATES_OF_REGISTRATION);
+  }
+
+  private getData<T>(query: TypedDocumentNode<unknown, unknown>): Observable<T> {
+    // eslint-disable-next-line prettier/prettier
+    return this.apollo.watchQuery({ query })
+      .valueChanges.pipe(
+        map((data) => data as T)
+      );
   }
 }
