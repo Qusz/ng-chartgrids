e6e5139035d62cc35f257a9c8f4be9338edf5a51
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-16 14:48:26 +0300

    re-export queries

---
+++ src/app/queries/index.ts
@@ -0,0 +1,6 @@
+import { GET_ALL_CUSTOMERS } from './get-all';
+import { GET_GENDERS } from './get-genders';
+import { GET_DATES_OF_REGISTRATION } from './get-dates-of-registration';
+import { GET_POINTS_OF_REGISTRATION } from './get-points-of-registration';
+
+export { GET_ALL_CUSTOMERS, GET_GENDERS, GET_POINTS_OF_REGISTRATION, GET_DATES_OF_REGISTRATION };
