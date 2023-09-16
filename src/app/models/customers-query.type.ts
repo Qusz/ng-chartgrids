5a11c12e126da92e66b560ebba3e28d10e6d36dd
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-16 14:46:57 +0300

    remove type

--- src/app/models/customers-query.type.ts
+++
@@ -1,9 +0,0 @@
-import type { Customer } from './customer.type';
-
-export type CustomersQuery = {
-  data: {
-    customers: Customer[] | null;
-  };
-  loading: boolean;
-  networkStatus: number;
-};
