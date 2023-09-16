7de15fe480350d31a84817ab2394e6b68014b87b
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-16 20:33:37 +0300

    import modules

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CardModule } from 'primeng/card';
+
+import { ChartModule } from 'src/app/components/chart/chart.module';
 import { ChartsComponent } from './charts.component';
 
 @NgModule({
   declarations: [ChartsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ChartModule, CardModule],
   exports: [ChartsComponent]
 })
 export class ChartsModule {}
