5f6ee97a38982395a33902a5bf04059104d0b8e0
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-16 20:36:33 +0300

    add default sidebar items

---
+++ src/app/components/sidebar/default-sidebar-items.ts
@@ -0,0 +1,12 @@
+import { SidebarItem } from 'src/app/models';
+
+export const defaultSidebarItems: SidebarItem[] = [
+  {
+    name: 'Charts',
+    route: 'charts'
+  },
+  {
+    name: 'Grids',
+    route: 'grids'
+  }
+];
