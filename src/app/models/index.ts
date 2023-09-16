e9d5f4d76e88d1d3b97776543bfd2deb0bb6cd22
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-16 14:48:38 +0300

    re-export types

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,15 +1,18 @@
 import type { ChartType } from './chart.type';
 import type { SidebarItem } from './sidebar-item.type';
 import type { Customer } from './customer.type';
-import type { CustomersQuery } from './customers-query.type';
-import type { ChartData, ChartDataCategory, ChartDataDate } from './chart-data.type';
+import type { CustomersQuery } from './query.type';
+import type { ChartDataCategory, ChartDataDate } from './chart-data.type';
+import type { CreateChartSettings } from './create-chart-settings.type';
+import type { DataProcessorSettings } from './data-processor-settings.type';
 
 export type {
   SidebarItem,
   Customer,
   CustomersQuery,
-  ChartData,
   ChartType,
   ChartDataCategory,
-  ChartDataDate
+  ChartDataDate,
+  CreateChartSettings,
+  DataProcessorSettings
 };
