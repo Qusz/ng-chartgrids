d4b94aa0e82089438ebaeaccf9fbe39f5d765832
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-17 20:50:21 +0300

    remove redundant imports

--- src/app/components/sidebar/sidebar.module.ts
+++ src/app/components/sidebar/sidebar.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
 
 @NgModule({
   declarations: [SidebarComponent],
-  imports: [CommonModule, ButtonModule, RouterModule],
+  imports: [CommonModule, RouterModule],
   exports: [SidebarComponent]
 })
 export class SidebarModule {}
