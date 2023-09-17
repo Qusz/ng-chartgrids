528a615ed11fae82752ee920fc34275f8f933b43
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-17 20:21:56 +0300

    add grid logic

--- src/app/pages/grids/grids.component.ts
+++ src/app/pages/grids/grids.component.ts
@@ -1,6 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+
+import type { AllUsersGrid } from 'src/app/models';
+
+import { Observable, map } from 'rxjs';
+import { GetDataService } from '../../shared/get-data/get-data.service';
+import { GridDataProcessorService } from '../../shared/grid-data-processor/grid-data-processor.service';
+
+import { allCustomersGridOptions } from './grids-options';
 
 @Component({
-  templateUrl: './grids.component.html'
+  templateUrl: './grids.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GridsComponent {}
+export class GridsComponent {
+  gridOptions = {
+    ...allCustomersGridOptions,
+    onGridReady: () => this.onGridReady()
+  };
+
+  rowData$!: Observable<AllUsersGrid[]>;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private getDataService: GetDataService,
+    private gridDataProcessorService: GridDataProcessorService
+  ) {}
+
+  private onGridReady() {
+    this.getGridData();
+  }
+
+  private getGridData() {
+    this.rowData$ = this.getDataService.getCustomers().pipe(
+      map(({ data }) => {
+        return data.customers
+          ? this.gridDataProcessorService.processAllCustomers(data.customers)
+          : [];
+      })
+    );
+
+    this.changeDetectorRef.markForCheck();
+  }
+}
