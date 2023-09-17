0f10f87ece80f04501842a17b28f6b5c773d2825
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-17 20:43:10 +0300

    remove redundant modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,21 @@
 /* eslint-disable no-param-reassign */
 
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { PrimeNGConfig } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { RippleModule } from 'primeng/ripple';
-
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
 import { DefaultLayoutModule } from './components/default-layout/default-layout.module';
 import { HeaderModule } from './components/header/header.module';
 import { SidebarModule } from './components/sidebar/sidebar.module';
 import { GraphQLModule } from './graphql.module';
 import { ChartsModule } from './pages/charts/charts.module';
 import { GridsModule } from './pages/grids/grids.module';
-
-const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
-  primeConfig.ripple = true;
-};
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,8 +24,6 @@ const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    ButtonModule,
-    RippleModule,
     DefaultLayoutModule,
     HeaderModule,
     SidebarModule,
@@ -42,14 +32,7 @@ const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
     ChartsModule,
     GridsModule
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeAppFactory,
-      deps: [PrimeNGConfig],
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
