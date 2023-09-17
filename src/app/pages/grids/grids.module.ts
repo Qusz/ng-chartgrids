894c151b0590a03272e806ff57982f19b4974ec6
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-17 20:19:51 +0300

    import module

--- src/app/pages/grids/grids.module.ts
+++ src/app/pages/grids/grids.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { GridModule } from '../../components/grid/grid.module';
 import { GridsComponent } from './grids.component';
 
 @NgModule({
   declarations: [GridsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, GridModule],
   exports: [GridsComponent]
 })
 export class GridsModule {}
