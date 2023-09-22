64ee9c8ba75e793ebf09908a3210518435beae8b
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-22 21:32:19 +0300

    update router imports

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { CardModule } from 'primeng/card';
 
 import { ChartModule } from 'src/app/components/chart/chart.module';
 import { ChartsComponent } from './charts.component';
 
+import { ChartsRoutingModule } from './charts-routing.module';
+
 @NgModule({
   declarations: [ChartsComponent],
-  imports: [CommonModule, ChartModule, CardModule],
+  imports: [CommonModule, RouterModule, ChartModule, CardModule, ChartsRoutingModule],
   exports: [ChartsComponent]
 })
 export class ChartsModule {}
