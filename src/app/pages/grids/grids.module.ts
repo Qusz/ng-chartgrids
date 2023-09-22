baa6eb43fb0b61628201a369587c6af95274460e
Denis Gorchakov <de.gorchakov@gmail.com>
2023-09-22 21:32:42 +0300

    update router imports

--- src/app/pages/grids/grids.module.ts
+++ src/app/pages/grids/grids.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { GridModule } from '../../components/grid/grid.module';
 import { GridsComponent } from './grids.component';
+import { GridsRoutingModule } from './grids-routing.module';
 
 @NgModule({
   declarations: [GridsComponent],
-  imports: [CommonModule, GridModule],
+  imports: [CommonModule, RouterModule, GridModule, GridsRoutingModule],
   exports: [GridsComponent]
 })
 export class GridsModule {}
